a238f346e4bded1f7b9bc1e22df067dadeb0e8b4
hoa1905 <94172304+hoa1905@users.noreply.github.com>
2023-03-28 13:58:28 +0700

    upload

---
+++ app/src/main/java/com/example/android/marsrealestate/network/MarsProperty.kt
@@ -0,0 +1,9 @@
+package com.example.android.marsrealestate.network
+
+import com.squareup.moshi.Json
+data class MarsProperty(
+        val id: String,
+        // used to map img_src from the JSON to imgSrcUrl in our class
+        @Json(name = "img_src") val imgSrcUrl: String,
+        val type: String,
+        val price: Double)
---
+++ app/src/main/java/com/example/android/marsrealestate/overview/OverviewFragment.kt
@@ -0,0 +1,38 @@
+package com.example.android.marsrealestate.overview
+
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.android.marsrealestate.R
+import com.example.android.marsrealestate.databinding.FragmentOverviewBinding
+
+class OverviewFragment : Fragment() {
+
+    private val viewModel: OverviewViewModel by lazy {
+        ViewModelProvider(this).get(OverviewViewModel::class.java)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        val binding = FragmentOverviewBinding.inflate(inflater)
+        //val binding = GridViewItemBinding.inflate(inflater)
+
+        // Allows Data Binding to Observe LiveData with the lifecycle of this Fragment
+        binding.lifecycleOwner = this
+
+        // Giving the binding access to the OverviewViewModel
+        binding.viewModel = viewModel
+
+        // Sets the adapter of the photosGrid RecyclerView
+        binding.photosGrid.adapter = PhotoGridAdapter()
+
+        setHasOptionsMenu(true)
+        return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.overflow_menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+}
